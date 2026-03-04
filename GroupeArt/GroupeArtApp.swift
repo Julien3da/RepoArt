3a162f8fe5f247e29b817e18c5cec53095d83c9d
Julien Estrada <julienestrada@mac.home>
2026-03-04 13:29:10 +0100

    Ajout du projet

---
+++ GroupeArt/GroupeArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupeArtApp.swift
+//  GroupeArt
+//
+//  Created by Julien Estrada on 04/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct GroupeArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
