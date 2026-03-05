3d0a5526ffe48daf11afa97445541a86f05cdfa7
Julien Estrada <julienestrada@mac.home>
2026-03-05 11:23:31 +0100

    Ajout du MVVM v2

---
+++ GroupeArt/Components/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  GroupeArt
+//
+//  Created by Julien Estrada on 05/03/2026.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test()
+}
