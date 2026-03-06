15e55f11a4b0d2ec7ba80bde3375dfca17e380ea
BlueOneThree <cdy69kb9vh@privaterelay.appleid.com>
2026-03-06 16:19:34 +0100

    added mockData, did the history track card, and added a view for track history

---
+++ GroupeArt/Components/DataBase.swift
@@ -0,0 +1,12 @@
+//
+//  DataBase.swift
+//  GroupeArt
+//
+//  Created by Julien Estrada on 05/03/2026.
+//
+
+import Foundation
+
+let user = User(username: "Fuve", userPic: nil, certification: true, userLocation: nil, followers: 1340, following: 60, countReviews: 130, bio: nil)
+
+var concert = [Concert(concertTitle: "Récitale de Rondoudou", artist: "Rondoudou", concertCover: "rondoudouCover", artistPicture: "rondoudouPicture", globalReview: 0.0, concertDate: "01/04/2026", concertLocation: "Tokyo")]
---
+++ GroupeArt/Components/HistoTrackCard.swift
@@ -0,0 +1,16 @@
+//  GroupeArt
+//
+//  Created by FUVE on 05/03/2026.
+//
+
+import SwiftUI
+
+struct HistoTrackCard: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    HistoTrackCard()
+}
