94b342d91a6c0c26fbd334c40de5037b3773bd8b
BlueOneThree <cdy69kb9vh@privaterelay.appleid.com>
2026-03-19 16:58:29 +0100

    fin de l'application

--- GroupeArt/Screens/FeedView.swift
+++ GroupeArt/Screens/FeedView.swift
@@ -43,83 +43,83 @@ struct FeedView: View {
 //                        }
 //                        .pickerStyle(.segmented)
                         
-                        Picker("Filtrer par type", selection: $feedTypeFilter) {
-                            
-                            HStack{
-                                Image(systemName: "play.circle")
-                                Text("Tout")
-                            } .tag(0)
-                            
-                            HStack{
-                                Image(systemName: "square.stack")
-                                Text("Albums")
-                            } .tag(1)
-                            
-                            HStack{
-                                Image(systemName: "music.note")
-                                Text("Morceaux")
-                            } .tag(2)
-                            
+//                        Picker("Filtrer par type", selection: $feedTypeFilter) {
+//                            
 //                            HStack{
-//                                Image(systemName: "music.note.house.fill")
-//                                Text("Concerts")
-//                            } .tag(3)
-                            
-                        } .accentColor(.orange)
-                            .accentColor(.orange)
-                            
-                            
-                        
-                        if viewModel.isLoading {
-                            ProgressView("Chargement du feed...")
-                                .frame(maxWidth: .infinity, alignment: .center)
-                        } else {
-                                let filteredReviews = viewModel.reviews.filter { review in
-                                    switch feedTypeFilter {
-                                        case 1: // Albums
-                                            return (review.album?.isEmpty == false)
-                                            
-                                        case 2: // Tracks
-                                            return (review.track?.isEmpty == false)
-                                            
-                                        default:
-                                            return true
-                                        }
-                            }
+//                                Image(systemName: "play.circle")
+//                                Text("Tout")
+//                            } .tag(0)
+//                            
+//                            HStack{
+//                                Image(systemName: "square.stack")
+//                                Text("Albums")
+//                            } .tag(1)
+//                            
+//                            HStack{
+//                                Image(systemName: "music.note")
+//                                Text("Morceaux")
+//                            } .tag(2)
+//                            
+////                            HStack{
+////                                Image(systemName: "music.note.house.fill")
+////                                Text("Concerts")
+////                            } .tag(3)
+//                            
+//                        } .accentColor(.orange)
+//                            .accentColor(.orange)
+//                            
+//                            
+//                        
+//                        if viewModel.isLoading {
+//                            ProgressView("Chargement du feed...")
+//                                .frame(maxWidth: .infinity, alignment: .center)
+//                        } else {
+//                                let filteredReviews = viewModel.reviews.filter { review in
+//                                    switch feedTypeFilter {
+//                                        case 1: // Albums
+//                                            return (review.album?.isEmpty == false)
+//                                            
+//                                        case 2: // Tracks
+//                                            return (review.track?.isEmpty == false)
+//                                            
+//                                        default:
+//                                            return true
+//                                        }
+//                            }
                             
                             FakeFeedCardView1()
                             FakeFeedCardView2()
                             FakeFeedCardView3()
                             FakeFeedCardView4()
                             
-                            LazyVStack {
-                                ForEach(filteredReviews) { review in
-                                    FeedItemView(
-                                        review: review,
-                                        albums: albumVM.albums,
-                                        tracks: trackVM.tracks,
-                                        artists: artistVM.artists
-                                    )
-                                }
-                            }
+//                            LazyVStack {
+//                                ForEach(filteredReviews) { review in
+//                                    FeedItemView(
+//                                        review: review,
+//                                        albums: albumVM.albums,
+//                                        tracks: trackVM.tracks,
+//                                        artists: artistVM.artists
+//                                    )
+//                                }
+//                            }
                             
-                        }
+//                        }
                     } .padding()
                 }
             } .navigationTitle("Feed")
         }
-        .task {
-
-
-            do {
-                _ = try await viewModel.fetchReviews()
-                _ = try await albumVM.fetchAlbums()
-                _ = try await trackVM.fetchTracks()
-                _ = try? await artistVM.fetchArtists()
-            } catch {
-                print("Erreur chargement feed : \(error)")
-            }
-        }
+//        .task {
+//
+//
+//            do {
+//                _ = try await viewModel.fetchReviews()
+//                _ = try await albumVM.fetchAlbums()
+//                _ = try await trackVM.fetchTracks()
+//                _ = try? await artistVM.fetchArtists()
+//            } catch {
+//                print("Erreur chargement feed : \(error)")
+//            }
+//        }
     }
 }
 
